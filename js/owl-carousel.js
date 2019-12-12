b9147a5170bd8d0bb9d03037477c46c6d6d1f9e6
Nursultan <nursultan.sharsheev@gmail.com>
2019-12-12 20:36:32 +0600

    update burger menu

---
+++ js/owl-carousel.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay:true,
+    autoplayTimeout: 5000,
+    autoplaySpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
