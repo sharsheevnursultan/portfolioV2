6d83435bfcd10f12f13cfc51e196831dfc8c03a5
Nursultan <nursultan.sharsheev@gmail.com>
2019-12-12 21:27:23 +0600

    update burger menu

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function burgerScript() {
+    let i = document.getElementById('burger');
+    if (i.classList.contains('nav-elements')){
+        i.classList.remove('nav-elements');
+        i.classList.add('menuVisible')
+    }else if(i.classList.contains('menuVisible')){
+        i.classList.remove('menuVisible');
+        i.classList.add('nav-elements')
+    }
+
+console.log('click')
+}
