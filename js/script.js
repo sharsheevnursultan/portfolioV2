c53aab7a23852f1025d3a3f60cd08ebb9f5ccad1
Nursultan <nursultan.sharsheev@gmail.com>
2020-01-16 20:49:58 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 function burgerScript() {
     let i = document.getElementById('burger');
-    if (i.classList.contains('nav-elements')){
+    if (i.classList.contains('nav-elements')) {
         i.classList.remove('nav-elements');
         i.classList.add('menuVisible')
-    }else if(i.classList.contains('menuVisible')){
+    } else if (i.classList.contains('menuVisible')) {
         i.classList.remove('menuVisible');
         i.classList.add('nav-elements')
     }
 
-console.log('click')
+    console.log('click')
 }
+
+
